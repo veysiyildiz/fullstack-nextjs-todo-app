048abaf58eb2c6580ce62e3d10d8b5a902f5cfb4
Veysi YILDIZ <veysiyildiz@gmail.com>
2023-11-30 00:50:10 +0300

    css fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={`${inter.className} bg-dark-1 flex justify-center items-center h-screen mt-3`}>{children}</body>
       </html>
     </ClerkProvider>
   );
