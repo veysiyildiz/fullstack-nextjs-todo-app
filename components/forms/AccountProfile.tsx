5fb71491d7757ee90aafed088131a4101644c058
Veysi YILDIZ <veysiyildiz@gmail.com>
2023-11-29 23:33:57 +0300

    fix fileUrl error

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -61,7 +61,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     if (hasImageChanged) {
       const imgRes = await startUpload(files);
 
-      if (imgRes && imgRes[0].fileUrl) {
+      if (imgRes && imgRes[0]?.fileUrl) {
         values.profile_photo = imgRes[0].fileUrl;
       }
     }
